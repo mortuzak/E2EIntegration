17b357873d1d560f69e2e13ccedf193a3288d325
Mortuza <Mortuza@mortuzas-mbp.lan1>
2018-08-23 19:38:25 -0400

    Added new Test Cases

---
+++ E2ESeleniumJenkins/src/testcases/TC_005.java
@@ -0,0 +1,26 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_005 {
+
+	@Test
+	public void testcase1 ()
+	{
+		System.setProperty("webdriver.chrome.driver", "/Users/Mortuza/Documents/Selenium/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		
+		String currentWindow = driver.getWindowHandle();
+		driver.switchTo().window(currentWindow);
+		
+		driver.get("https://www.facebook.com/");
+		driver.findElement(By.id("email")).sendKeys("whosyourdaddy");
+		driver.findElement(By.id("pass")).sendKeys("whosyourmommy");
+		driver.quit();
+		
+	}
+	
+}
